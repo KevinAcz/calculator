00985f3693004deb3ede01776d36100707772deb
KevinAcz <kevinaceitonsalazar@gmail.com>
2023-08-20 19:08:00 -0400

    Branch for fix logic

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
-let numberOne = 0;
-let numberTwo = 0;
+let currentNumber = '';
+let firstNumber = '';
+let secondNumber = '';
 let operator = '';
 
 function add(x,y) {
@@ -28,3 +29,56 @@ function operate(x, y, operator) {
         return ( divide(x,y));
     }
 }
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll(".buttons-container button");
+console.log(buttons);
+
+buttons.forEach((button) => {
+    button.addEventListener('click', function (e) {
+
+        console.log(e.target);
+        //if button is CLEAR
+        if (button.textContent === 'Clear') {
+            display.textContent = 'Display';
+            currentNumber = '';
+            firstNumber = '';
+            secondNumber = '';
+        } 
+
+        //IF button is an operator,
+        else if (isNaN(Number(button.textContent)) && operator !== '=') {
+
+            let result = 0;
+            
+            operator = button.textContent;
+
+
+            if (firstNumber === '') {
+                firstNumber = currentNumber;
+            } else if (secondNumber === ''){ 
+                secondNumber = currentNumber;
+            }
+
+            //IF both numbers exists, we can operate:
+            if (firstNumber !== '' && secondNumber !== '') {
+            
+            if (operator !== '') {
+                result = operate(+firstNumber,+secondNumber, operator);
+            }      
+            operator = '';
+
+            display.textContent = result;
+
+            firstNumber = result;
+            secondNumber = '';
+            }
+            currentNumber = '';
+
+        } else { //if Button is a number
+            currentNumber += button.textContent;
+            display.textContent = currentNumber;
+        }
+
+        console.log(currentNumber);
+    })
+});
